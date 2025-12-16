22a77ba28860c692f7a6a6442e8b5d76a0bf1659
Priyagupta108 <priyagupta108@github.com>
2025-12-16 17:31:09 +0530

    rerun

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module mywebapp
 go 1.18
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
 )
--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
     "fmt"
     "net/http"
+
     "github.com/gorilla/mux"
+    "github.com/google/uuid"
 )
 
 func main() {
     r := mux.NewRouter()
 
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    
-        fmt.Fprintf(w, "Hello, Web!")
+        id := uuid.NewString()
+        fmt.Fprintf(w, "Hello, Web! UUID: %s", id)
     })
     http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
