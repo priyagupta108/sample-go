9ef18fcd2630995cd705d07534c486637c97f605
Priya Gupta <147705955+priyagupta108@users.noreply.github.com>
2026-02-25 15:48:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
     "github.com/gorilla/mux"
 )
 
+
 func main() {
     r := mux.NewRouter()
-
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     
         fmt.Fprintf(w, "Hello, Web!")
